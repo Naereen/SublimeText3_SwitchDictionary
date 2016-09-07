d2f370e6764fedc51d142d87bfd4dc29b986a66b
Lilian Besson <lbesson@ens-cachan.fr>
2016-09-08 01:57:08 +0200

    First version?

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
-# SublimeText3 SwitchDictionary
-It is a tiny [Sublime Text 3]() plugin, not tested on ST 2, that add commands and shortcuts to switch between: no spellcheck, spellcheck for French, and spellcheck for English.
+# SublimeText3 SwitchDictionary - ALPHA STAGE
+## WARNING
+This plug-in is still in active development!
+
+## About
+It is a tiny [Sublime Text 3](https://www.sublimetext.com/3) plug-in, **not tested** on [ST 2](https://www.sublimetext.com/), that add commands and shortcuts to switch between: no spell-check, spell-check for *French*, and spell-check for *English*.
 
 *Note:* it will soon be better, allowing you to define your own list of dictionaries.
 
 ## Commands
 ### `enable_english_spellcheck`
-  - It enables shellcheck, and sets the dictionary to *English*
-  - [It is binded]() to the key chain <key>ctrl+k, ctrl+e</key>
+  - It enables shell-check, and sets the dictionary to *English*
+  - [It is binded](Default.sublime-keymap) to the key chain <key>ctrl+k, ctrl+e</key>
 
 ### `enable_french_spellcheck`
-  - It enables shellcheck, and sets the dictionary to *French*
-  - [It is binded]() to the key chain <key>ctrl+k, ctrl+f</key>
+  - It enables shell-check, and sets the dictionary to *French*
+  - [It is binded](Default.sublime-keymap) to the key chain <key>ctrl+k, ctrl+f</key>
 
 ### `switch_spellcheck`
   - It cycles between *English*, *French*, and *None*
-  - [It is binded]() to the key chain <key>ctrl+k, ctrl+s</key> and <key>ctrl+alt+s</key>
+  - [It is binded](Default.sublime-keymap) to the key chain <key>ctrl+k, ctrl+s</key> and also to <key>ctrl+alt+s</key>
 
 ### `disable_spellcheck`
   - It disables shellcheck
@@ -23,18 +27,40 @@ It is a tiny [Sublime Text 3]() plugin, not tested on ST 2, that add commands an
 ----
 
 ## Accessing commands through...
-### The palette
-Search for 
+### :notebook: The *Command Palette*
+Press <key>ctrl+shift+p</key> (Windows, Linux) or <key>cmd+shift+p</key> (OS X) to open the *Command Palette*, and then search for:
+
+ - `Switch Dictionary: to English`,
+ - `Switch Dictionary: to French`,
+ - `Switch Dictionary: None ↔ French ↔ English`.
+
+### :mouse: The *Encoding Menu*
+In the status bar, you should have a menu showing the encoding of the current file (by default it is `UTF-8`).
+Click on the menu, and you can *click* on:
+
+ - `Switch Dictionary – to English`
+ - `Switch Dictionary – to French`
+ - `Switch Dictionary – None ↔ French ↔ English`
+
+### :musical_keyboard: Shortcuts
+[By default](Default.sublime-keymap), the following shortcuts are available
+
+ - <key>ctrl+k, ctrl+e</key> : `Switch Dictionary – to English`
+ - <key>ctrl+k, ctrl+f</key> : `Switch Dictionary – to French`
+ - <key>ctrl+k, ctrl+s</key>, <key>ctrl+alt+s</key> : `Switch Dictionary – None ↔ French ↔ English`
+
 
 ----
 
-## Install
-### With [Package Control](https://packagecontrol.io/)
-If you have [Package Control](https://packagecontrol.io/) installed in Sublime Text 2/3, just press <key>ctrl+shift+p</key> (Windows, Linux) or <key>cmd+shift+p</key> (OS X) to open the Command Pallete.
+## :question: How to install it ?
+### :ok_hand: With [Package Control](https://packagecontrol.io/)
+If you have [Package Control](https://packagecontrol.io/) installed in Sublime Text 2/3, just press <key>ctrl+shift+p</key> (Windows, Linux) or <key>cmd+shift+p</key> (OS X) to open the *Command Palette*.
 
-Start typing `install` to select `Package Control: Install Package`, then search for `SwitchDictionary` and select it. That's it.
+Start typing `install` to select `Package Control: Install Package`, then search for `SwitchDictionary` and select it. That's it!
 
-### Manually, with git
+You can check it was well installed by hitting <key>ctrl+k, ctrl+s</key> (it should enable English spell checking)
+
+### :floppy_disk: Manually, with [git](https://git-scm.com/)
 You can also install this package manually by entering the Packages directory of Sublime Text 2/3 and issuing on a terminal:
 
     git clone https://github.com/Naereen/SublimeText3_SwitchDictionary
@@ -42,8 +68,8 @@ You can also install this package manually by entering the Packages directory of
 ----
 
 ## :boom: TODO
-- Publish it !
-- Find a nice way to install it locally?
+- Publish it on [Package Control](https://packagecontrol.io/) !
+- Find a nice way to install it locally? Or is it FIXED already?
 
 ## :hourglass: Future Features
 - Be more general: allow users to define manually a list of dictionary (path, and name), and add one command for each dictionary, and make the switch_spellcheck command cycle between them!
@@ -51,7 +77,7 @@ You can also install this package manually by entering the Packages directory of
 ----
 
 ### :scroll: License
-This plugin is published under the terms of the [MIT license](http://lbesson.mit-license.org/) (file [LICENSE.txt](LICENSE.txt)).
+This plug-in is published under the terms of the [MIT license](http://lbesson.mit-license.org/) (file [LICENSE.txt](LICENSE.txt)).
 © [Lilian Besson](https://github.com/Naereen), 2016.
 
 [![Ask Me Anything](https://img.shields.io/badge/ask%20me-anything-1abc9c.svg)](https://github.com/Naereen/ama)
--- switch_dicts.py
+++ switch_dicts.py
@@ -21,7 +21,7 @@
 
 About:
 
-- *Date:*    2016-09-07
+- *Date:*    2016-09-08
 - *Web:*     https://github.com/Naereen/SublimeText3_SwitchDictionary/
 - *Author:*  Lilian Besson (C) 2016
 - *Licence:* MIT Licence (http://lbesson.mit-license.org)
@@ -43,7 +43,7 @@ class DisableSpellcheckCommand(sublime_plugin.TextCommand):
         # 1. Display a message in the status bar
         self.view.set_status("Spelling", "")
         # 2. Disable spell check
-        print("Setting 'spell_check' to 'false' ...")
+        print("Setting 'spell_check' to 'false' ...")  # DEBUG
         self.view.run_command("set_setting", {
             "setting": "spell_check",
             "value": False
@@ -61,7 +61,7 @@ class EnableEnglishSpellcheckCommand(sublime_plugin.TextCommand):
         sublime.status_message("Switching spelling to English...")
 
         # 1. Load the good dict
-        print("Setting 'dictionary' setting to '{}' ...".format(join("Packages", path_to_the_dict_en)))
+        print("Setting 'dictionary' setting to '{}' ...".format(join("Packages", path_to_the_dict_en)))  # DEBUG
         # try:
         self.view.run_command("set_setting", {
             "setting": "dictionary",
@@ -75,7 +75,7 @@ class EnableEnglishSpellcheckCommand(sublime_plugin.TextCommand):
         # TODO: implement here a smart detection : if "Language - English/en_US.dic" is not here, load "Dictionaries/en_US.dic" instead
 
         # 2. Enable spell check
-        print("Setting 'spell_check' to 'true' ...")
+        print("Setting 'spell_check' to 'true' ...")  # DEBUG
         self.view.run_command("set_setting", {
             "setting": "spell_check",
             "value": True
@@ -83,20 +83,6 @@ class EnableEnglishSpellcheckCommand(sublime_plugin.TextCommand):
 
         # 3. Display a message in the status bar
         self.view.set_status("Spelling", "English")
-
-        # 4. Display a system notification (XXX experimental)
-        # self.view.window().run_command("exec", {
-        #     "cmd": [
-        #         # "/usr/bin/env",
-        #         # "/usr/bin/notify-send",  # XXX put absolute path?
-        #         "notify-send",  # XXX put absolute path?
-        #         "--icon", "/opt/sublime_text/Icon/256x256/sublime-text.png",  # icon
-        #         "--urgency", "low",  # urgency level
-        #         "--expire-time", "500",  # duration in milli-seconds
-        #         "Sublime Text 3",  # title
-        #         " - <b>Spell check</b> <i>enabled</i>\n - <b>Switched dictionary</b> to <b>English (\"<i>{}</i>\")".format(path_to_the_dict_en),  # body
-        #     ]
-        # })
 
         print("Done running the command 'enable_english_spellcheck' ...")  # DEBUG
 
@@ -111,7 +97,7 @@ class EnableFrenchSpellcheckCommand(sublime_plugin.TextCommand):
         sublime.status_message("Switching spelling to French...")
 
         # 1. Load the good dict
-        print("Setting 'dictionary' setting to '{}' ...".format(join("Packages", path_to_the_dict_fr)))
+        print("Setting 'dictionary' setting to '{}' ...".format(join("Packages", path_to_the_dict_fr)))  # DEBUG
         # try:
         self.view.run_command("set_setting", {
             "setting": "dictionary",
@@ -125,7 +111,7 @@ class EnableFrenchSpellcheckCommand(sublime_plugin.TextCommand):
         # TODO: implement here a smart detection : if "Language - French - Français/fr_FR.dic" is not here, load "Dictionaries/French.dic" instead
 
         # 2. Enable spell check
-        print("Setting 'spell_check' to 'true' ...")
+        print("Setting 'spell_check' to 'true' ...")  # DEBUG
         self.view.run_command("set_setting", {
             "setting": "spell_check",
             "value": True
@@ -133,20 +119,6 @@ class EnableFrenchSpellcheckCommand(sublime_plugin.TextCommand):
 
         # 3. Display a message in the status bar
         self.view.set_status("Spelling", "French")
-
-        # 4. Display a system notification (XXX experimental)
-        # self.view.run_command("exec", {
-        #     "cmd": [
-        #         # "/usr/bin/env",
-        #         # "/usr/bin/notify-send",  # XXX put absolute path?
-        #         "notify-send",  # XXX put absolute path?
-        #         "--icon", "/opt/sublime_text/Icon/256x256/sublime-text.png",  # icon
-        #         "--urgency", "low",  # urgency level
-        #         "--expire-time", "500",  # duration in milli-seconds
-        #         "Sublime Text 3",  # title
-        #         " - <b>Spell check</b> <i>enabled</i>\n - <b>Switched dictionary</b> to <b>French (\"<i>{}</i>\")".format(path_to_the_dict_fr),  # body
-        #     ]
-        # })
 
         print("Done running the command 'enable_french_spellcheck' ...")  # DEBUG
 
@@ -159,7 +131,7 @@ class SwitchSpellcheckCommand(sublime_plugin.TextCommand):
             status = self.view.get_status("Spelling")
         except:
             status = None
-        print("Status for key='Spelling' =", status)
+        print("Status for key='Spelling' =", status)  # DEBUG
 
         if not status or status == '':  # Default
             sublime.status_message("Switching spelling from {} to English...".format(status))
@@ -171,7 +143,7 @@ class SwitchSpellcheckCommand(sublime_plugin.TextCommand):
             sublime.status_message("Switching spelling from {} to None...".format(status))
             self.view.run_command("disable_spellcheck")
         else:
-            print("Warning (switch_dicts.py from SwitchDictionary plugin): unknown value for 'status'")
+            print("Warning (switch_dicts.py from SwitchDictionary plugin): unknown value for 'status'")  # DEBUG
 
         print("Done running the command 'switch_spellcheck' ...")  # DEBUG
 
