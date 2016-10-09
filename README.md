f6e81e522f7cc549725e5595c16b28b7ad4ab33f
Lilian Besson <lbesson@ens-cachan.fr>
2016-10-09 14:40:13 +0200

    New version 0.0.3 : adds the 'auto_switch_spellcheck' command.
    - disable_spellcheck is also in the menu/palette now (but not key-binded)
    - auto_switch_spellcheck works ONLY to know if it is French / non French
    - XXX raw include of the required module, https://github.com/Mimino666/langdetect, thanks @Mimino666

--- README.md
+++ README.md
@@ -1,84 +1,98 @@
-# [SublimeText3 SwitchDictionary](https://github.com/Naereen/SublimeText3_SwitchDictionary/) - ALPHA STAGE
+# [SublimeText3 SwitchDictionary](https://github.com/Naereen/SublimeText3_SwitchDictionary/) - BETA STAGE
 ## WARNING
-[This plug-in](https://github.com/Naereen/SublimeText3_SwitchDictionary/) [is still in active development](https://github.com/Naereen/SublimeText3_SwitchDictionary/graphs/commit-activity) and in alpha stage!
+[This plug-in](https://github.com/Naereen/SublimeText3_SwitchDictionary/) [is still in active development](https://github.com/Naereen/SublimeText3_SwitchDictionary/graphs/commit-activity) and in very early stage!
 
 ## About
 It is a tiny [Sublime Text 3](https://www.sublimetext.com/3) plug-in (not tested on [ST 2](https://www.sublimetext.com/)), that add commands and shortcuts to switch between: no spell-check, spell-check for *French*, and spell-check for *English*.
 
 *Note:* I hope it will soon be better, allowing you to define your own list of dictionaries.
 
-## Demo (screencast)
+## Demo (gif screencast)
 ![demo of Sublime Text 3 'SwitchDictionary' plugin (gif)](demo_SublimeText3_SwitchDictionary_plugin.gif)
 
 ----
 
 ## Commands
 ### `enable_english_spellcheck`
-  - It enables shell-check, and sets the dictionary to *English*
+  - It enables spell-check, and sets the dictionary to *English*
   - [It is binded](Default.sublime-keymap) to the key chain <kbd>ctrl+k, ctrl+e</kbd>
 
 ### `enable_french_spellcheck`
-  - It enables shell-check, and sets the dictionary to *French*
+  - It enables spell-check, and sets the dictionary to *French*
   - [It is binded](Default.sublime-keymap) to the key chain <kbd>ctrl+k, ctrl+f</kbd>
 
 ### `switch_spellcheck`
   - It cycles between *English*, *French*, and *None*
-  - [It is binded](Default.sublime-keymap) to the key chain <kbd>ctrl+k, ctrl+s</kbd> and also to <kbd>ctrl+alt+s</kbd>
+  - [It is binded](Default.sublime-keymap) to the key chain <kbd>ctrl+k, ctrl+s</kbd> and also to <kbd>ctrl+alt+s</kbd> (*s* for *switch*)
+
+### `auto_switch_spellcheck`
+  - It uses [langdetect.detect](https://github.com/Mimino666/langdetect) to (try to) automatically detect the language of the current file
+  - Right now, the detected language is only used to know if it is French / or not French (English or other language). TODO I need to generalize this
+  - It sets to *French* if the file is in French (or as a high probability of being written in French), and sets to *English* otherwise
+  - [It is binded](Default.sublime-keymap) to <kbd>ctrl+alt+a</kbd> (*a* for *auto*)
 
 ### `disable_spellcheck`
-  - It disables shellcheck
+  - It simply disables the spell-check
   - It is NOT binded to any key (*tip:* use the default command, binded to <kbd>F6</kbd>)
 
 ----
 
 ## Accessing commands through...
 ### :notebook: The *Command Palette* !
-Press <kbd>ctrl+shift+p</kbd> (Windows, Linux) or <kbd>cmd+shift+p</kbd> (OS X) to open the *Command Palette*, and then search for:
+Press <kbd>ctrl+shift+p</kbd> (Windows, Linux) or <kbd>cmd+shift+p</kbd> (OS X) to open the [*Command Palette*](SwitchDictionary.sublime-commands), and then search for:
 
+ - `Switch Dictionary: disable (None)`,
  - `Switch Dictionary: to English`,
  - `Switch Dictionary: to French`,
  - `Switch Dictionary: None ↔ French ↔ English`.
+ - `Auto Switch Dictionary (guess)`.
 
 ### :mouse: The *Encoding Menu* !
 In the status bar, you should have a menu showing the encoding of the current file (by default it is `UTF-8`).
-Click on the menu, and you can *click* on:
+Click on [the menu](Encoding.sublime-menu), and you can *click* on:
 
+ - `Switch Dictionary – Disable (None)`
  - `Switch Dictionary – to English`
  - `Switch Dictionary – to French`
  - `Switch Dictionary – None ↔ French ↔ English`
+ - `Auto Switch Dictionary (guess)`
 
 ### :musical_keyboard: *Shortcuts* !
 [By default](Default.sublime-keymap), the following shortcuts are available
 
  - <kbd>ctrl+k, ctrl+e</kbd> : `Switch Dictionary – to English`
  - <kbd>ctrl+k, ctrl+f</kbd> : `Switch Dictionary – to French`
- - <kbd>ctrl+k, ctrl+s</kbd>, <kbd>ctrl+alt+s</kbd> : `Switch Dictionary – None ↔ French ↔ English`
+ - <kbd>ctrl+k, ctrl+s</kbd> or <kbd>ctrl+alt+s</kbd> : `Switch Dictionary – None ↔ French ↔ English`
+ - <kbd>ctrl+alt+a</kbd> : `Auto Switch Dictionary (guess)`
 
 ----
 
 ## :question: How to install it ?
-### :ok_hand: With [Package Control](https://packagecontrol.io/) : *NOT YET !*
+### :ok_hand: With [Package Control](https://packagecontrol.io/) : FIXME *NOT YET !*
 If you have [Package Control](https://packagecontrol.io/) installed in Sublime Text 2/3, just press <kbd>ctrl+shift+p</kbd> (Windows, Linux) or <kbd>cmd+shift+p</kbd> (OS X) to open the *Command Palette*.
 
-Start typing `install` to select `Package Control: Install Package`, then search for `SwitchDictionary` and select it. That's it!
+1. Start typing `install` to select `Package Control: Install Package`,
+2. Then search for `SwitchDictionary` and select it. That's it!
 
 ### :floppy_disk: Manually, with [git](https://git-scm.com/)
-You can also install this package manually by entering the Packages directory of Sublime Text 2/3 and issuing on a terminal:
+You can also install this package manually by entering the Packages directory of Sublime Text 3 (through the menu: "Preferences" > "Browse packages"), and issuing this command on a terminal:
 
 ```bash
-cd ~/.config/sublime-text-3/Packages/  # adapt on Windows or Mac OS X
+cd ~/.config/sublime-text-3/Packages/  # adapt the path on Windows or Mac OS X
 git clone https://github.com/Naereen/SublimeText3_SwitchDictionary
 ```
 
 ### Check it
-You can check it was well installed by hitting <kbd>ctrl+k, ctrl+s</kbd> (it should enable English spell checking)
+You can check it was well installed by hitting <kbd>ctrl+k, ctrl+s</kbd> (it should enable English spell checking).
 
 ----
 
 ## :boom: [TODO !](https://github.com/Naereen/SublimeText3_SwitchDictionary/projects/1)
-- [ ] Finish it...
 - [X] Test it on another laptop : DONE on another Linux laptop
 - [ ] Test it on Mac OS X and/or Windows ??
+- [X] Implement a smart detection, to know if a file is in French or in English
+- [ ] Be more general, allow a user to define his own favorite dictionaries
+- [ ] Finish it
 - [ ] Publish it on [Package Control](https://packagecontrol.io/) !
 
 ## :hourglass: [Future Features](https://github.com/Naereen/SublimeText3_SwitchDictionary/projects/1)
--- switch_dicts.py
+++ switch_dicts.py
@@ -4,7 +4,7 @@
 
 'disable_spellcheck'
   - It disables shellcheck
-  - It is NOT binded to any key
+  - It is NOT binded to any key (use F6 instead)
 
 'enable_english_spellcheck'
   - It enables shellcheck and sets the dictionary to English
@@ -18,46 +18,94 @@
   - It cycles between English, French, and None
   - It is binded to the key chain ["ctrl+k", "ctrl+s"] and ["ctrl+alt+s"]
 
+'auto_switch_spellcheck'
+  - It guesses the more probable language (between French and English)
+  - It sets the dictionary to English or French, or English by default if it fails?
+  - It is binded to the ["ctrl+alt+a"]
+
 TODO 'set_english_email'
   - It enables shellcheck, and sets the dictionary to English
   - It sets the syntax to "Markdown GFM"
-  - And ask to save the file
+  - And ask to save the file ?
 
 TODO 'set_french_email'
   - It enables shellcheck, and sets the dictionary to French
   - It sets the syntax to "Markdown GFM"
-  - And ask to save the file
+  - And ask to save the file ?
 
 
 About:
 
-- *Date:*    2016-09-08
+- *Date:*    2016-10-09
+- *Version:* 0.0.3
 - *Web:*     https://github.com/Naereen/SublimeText3_SwitchDictionary/
 - *Author:*  Lilian Besson (C) 2016
 - *Licence:* MIT Licence (http://lbesson.mit-license.org)
 """
 
+# 1. Import requirements
 from __future__ import print_function, division  # Python 2 compatibility if needed
+# from __future__ import absolute_import  # XXX useful ?
 
 import sublime
 import sublime_plugin
 
-from os.path import join
+import sys
+import os.path
 
+PLUGIN_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, PLUGIN_DIR)
+
+
+# Make sure nothing gets broken even if the package langdetect is not available
+# Note: It comes from https://github.com/Mimino666/langdetect
+try:
+    from langdetect import detect
+    # print("Success when importing 'detect' from 'langdetect' (coming from https://github.com/Mimino666/langdetect) ...")   # DEBUG
+except ImportError:
+    def detect(*args):
+        return "en"
+    print("[ERROR] Failed to import langdetect : it will always detect English, by default.")
+    # print("Are you sure you have installed the SwitchDictionary plugin from packagecontrol.io or from GitHub ?")  # DEBUG
+
+
+# 2. Utility functions
+def spell_check(self, b):
+    """ Set the 'spell_check' setting to the boolean b (True or False).
+    """
+    print("Setting 'spell_check' to '%s' ..." % b)  # DEBUG
+    self.view.run_command("set_setting", {
+        "setting": "spell_check",
+        "value": b
+    })
+
+
+def set_setting_dictionary(self, path_to_dict):
+    """ Set the 'dictionary' setting to the boolean path 'path_to_dict'.
+    """
+    print("Setting 'dictionary' setting to '{}' ...".format(
+        os.path.join("Packages", path_to_dict)
+    ))  # DEBUG
+    self.view.run_command("set_setting", {
+        "setting": "dictionary",
+        "value": os.path.join("Packages", path_to_dict)
+    })
+
+
+# 3. sublime_plugin.TextCommand classes : commands !
 
 class DisableSpellcheckCommand(sublime_plugin.TextCommand):
+    """ TextCommand for ST3, disabling the spell check. """
     def run(self, edit):
         print("\nStarting to run the command 'disable_spellcheck' ...")  # DEBUG
         sublime.status_message("Disabling spelling...")
 
         # 1. Display a message in the status bar
         self.view.set_status("Spelling", "")
+
         # 2. Disable spell check
-        print("Setting 'spell_check' to 'false' ...")  # DEBUG
-        self.view.run_command("set_setting", {
-            "setting": "spell_check",
-            "value": False
-        })
+        spell_check(self, False)
+
         print("Done running the command 'disable_spellcheck' ...")  # DEBUG
 
 
@@ -65,31 +113,39 @@ path_to_the_dict_en = "Language - English/en_US.dic"
 # path_to_the_dict_en = "Dictionaries/en_US.dic"
 
 
+def set_english(self):
+    """ Small function to set the dictionary to English and enable spell check."""
+    self.view.run_command("set_setting", {
+        "setting": "dictionary",
+        "value": os.path.join("Packages", path_to_the_dict_en)
+    })
+    spell_check(self, True)
+
+
 class EnableEnglishSpellcheckCommand(sublime_plugin.TextCommand):
+    """ TextCommand for ST3, enabling the spell check to 'English'. """
     def run(self, edit):
         print("\nStarting to run the command 'enable_english_spellcheck' ...")  # DEBUG
         sublime.status_message("Switching spelling to English...")
 
         # 1. Load the good dict
-        print("Setting 'dictionary' setting to '{}' ...".format(join("Packages", path_to_the_dict_en)))  # DEBUG
+        print("Setting 'dictionary' setting to '{}' ...".format(os.path.join("Packages", path_to_the_dict_en)))  # DEBUG
         # try:
-        self.view.run_command("set_setting", {
-            "setting": "dictionary",
-            "value": join("Packages", path_to_the_dict_en)
-        })
+        # self.view.run_command("set_setting", {
+        #     "setting": "dictionary",
+        #     "value": os.path.join("Packages", path_to_the_dict_en)
+        # })
         # except:
         #     self.view.run_command("set_setting", {
         #         "setting": "dictionary",
-        #         "value": join("Packages", "Dictionaries/en_US.dic")
+        #         "value": os.path.join("Packages", "Dictionaries/en_US.dic")
         #     })
         # TODO: implement here a smart detection : if "Language - English/en_US.dic" is not here, load "Dictionaries/en_US.dic" instead
 
+        set_english(self)
+
         # 2. Enable spell check
-        print("Setting 'spell_check' to 'true' ...")  # DEBUG
-        self.view.run_command("set_setting", {
-            "setting": "spell_check",
-            "value": True
-        })
+        # spell_check(self, True)
 
         # 3. Display a message in the status bar
         self.view.set_status("Spelling", "English")
@@ -101,31 +157,39 @@ path_to_the_dict_fr = "Language - French - Français/fr_FR.dic"
 # path_to_the_dict_fr = "Dictionaries/French.dic"
 
 
+def set_french(self):
+    """ Small function to set the dictionary to French and enable spell check."""
+    self.view.run_command("set_setting", {
+        "setting": "dictionary",
+        "value": os.path.join("Packages", path_to_the_dict_fr)
+    })
+    spell_check(self, True)
+
+
 class EnableFrenchSpellcheckCommand(sublime_plugin.TextCommand):
+    """ TextCommand for ST3, enabling the spell check to 'French'. """
     def run(self, edit):
         print("\nStarting to run the command 'enable_french_spellcheck' ...")  # DEBUG
         sublime.status_message("Switching spelling to French...")
 
         # 1. Load the good dict
-        print("Setting 'dictionary' setting to '{}' ...".format(join("Packages", path_to_the_dict_fr)))  # DEBUG
+        print("Setting 'dictionary' setting to '{}' ...".format(os.path.join("Packages", path_to_the_dict_fr)))  # DEBUG
         # try:
-        self.view.run_command("set_setting", {
-            "setting": "dictionary",
-            "value": join("Packages", path_to_the_dict_fr)
-        })
+        # self.view.run_command("set_setting", {
+        #     "setting": "dictionary",
+        #     "value": os.path.join("Packages", path_to_the_dict_fr)
+        # })
         # except:
         #     self.view.run_command("set_setting", {
         #         "setting": "dictionary",
-        #         "value": join("Packages", "Dictionaries/French.dic")
+        #         "value": os.path.join("Packages", "Dictionaries/French.dic")
         #     })
         # TODO: implement here a smart detection : if "Language - French - Français/fr_FR.dic" is not here, load "Dictionaries/French.dic" instead
 
+        set_french(self)
+
         # 2. Enable spell check
-        print("Setting 'spell_check' to 'true' ...")  # DEBUG
-        self.view.run_command("set_setting", {
-            "setting": "spell_check",
-            "value": True
-        })
+        # spell_check(self, True)
 
         # 3. Display a message in the status bar
         self.view.set_status("Spelling", "French")
@@ -134,28 +198,84 @@ class EnableFrenchSpellcheckCommand(sublime_plugin.TextCommand):
 
 
 class SwitchSpellcheckCommand(sublime_plugin.TextCommand):
+    """ TextCommand for ST3, switching between 'English', 'French' and None spell check. """
     def run(self, edit):
         print("\nStarting to run the command 'switch_spellcheck' ...")  # DEBUG
 
         try:
             status = self.view.get_status("Spelling")
+            if status == '':
+                status = None
         except:
             status = None
         print("Status for key='Spelling' =", status)  # DEBUG
 
         if not status or status == '':  # Default
             sublime.status_message("Switching spelling from {} to English...".format(status))
-            self.view.run_command("enable_english_spellcheck")
+            # self.view.run_command("enable_english_spellcheck")
+            set_english(self)
+            self.view.set_status("Spelling", "English")
         elif status == 'English':
             sublime.status_message("Switching spelling from {} to French...".format(status))
-            self.view.run_command("enable_french_spellcheck")
+            # self.view.run_command("enable_french_spellcheck")
+            set_french(self)
+            self.view.set_status("Spelling", "French")
         elif status == 'French':
             sublime.status_message("Switching spelling from {} to None...".format(status))
-            self.view.run_command("disable_spellcheck")
+            # self.view.run_command("disable_spellcheck")
+            spell_check(self, False)
+            self.view.set_status("Spelling", "")
         else:
             print("Warning (switch_dicts.py from SwitchDictionary plugin): unknown value for 'status'")  # DEBUG
 
         print("Done running the command 'switch_spellcheck' ...")  # DEBUG
+
+
+class AutoSwitchSpellcheckCommand(sublime_plugin.TextCommand):
+    """ TextCommand for ST3, automatically switching between 'English', 'French' and None spell check. """
+    def run(self, edit):
+        print("\nStarting to run the command 'auto_switch_spellcheck' ...")  # DEBUG
+
+        # 1. get full content, 'content' of the current file, a unicode Python string
+        # 2. run detect(content), gets language code
+        # 3. XXX currently, if it is 'fr', sets to French, otherwise sets to English
+        detection_success = True
+        try:
+            with open(self.view.file_name(), 'r') as current_file:
+                print("I am read the content of the current file, '%s' ..." % current_file.name)
+                content_of_current_file = ''.join(current_file.readlines())
+                detected_language = detect(content_of_current_file)
+                print("I detected that the current file is apparently written in '%s' ..." % detected_language)
+        except:
+            detected_language = 'en'
+            detection_success = False
+            print("[ERROR] I failed to open, read or detect the language, using 'en', default language is English ...")
+
+        if detection_success and detected_language == 'fr':
+            sublime.status_message("Switching spelling to French (guessed)...")
+            # self.view.run_command("enable_french_spellcheck")
+            self.view.set_status("Spelling", "French")
+            set_french(self)
+        elif detection_success and detected_language != 'en':
+            # FIXME implement this in general!
+            print("[ERROR] I detected that the current file is in '%s', but I don't know how to set spell check for this language." % detected_language)
+            print("==> Do it manually !")
+            sublime.status_message("Switching spelling to English (failure, non supported language)...")
+            # self.view.run_command("enable_english_spellcheck")
+            set_english(self)
+            self.view.set_status("Spelling", "English")
+        elif detection_success and detected_language == 'en':
+            sublime.status_message("Switching spelling to English (guessed)...")
+            # self.view.run_command("enable_english_spellcheck")
+            set_english(self)
+            self.view.set_status("Spelling", "English")
+        else:
+            sublime.status_message("Switching spelling to English (default)...")
+            # self.view.run_command("enable_english_spellcheck")
+            set_english(self)
+            self.view.set_status("Spelling", "English")
+
+        print("Done running the command 'auto_switch_spellcheck' ...")  # DEBUG
 
 
 # End of switch_dicts.py
